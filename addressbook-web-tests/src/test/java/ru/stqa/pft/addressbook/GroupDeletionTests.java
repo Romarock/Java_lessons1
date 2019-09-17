7934f507a02afbb572966767d24a211d60832468
Romarock <55085243+Romarock@users.noreply.github.com>
2019-09-17 13:20:58 +0300

    Добавлен тест GroupDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupDeletionTests extends TestBase {
+
+
+
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+
+    wd.findElement(By.linkText("groups")).click();
+    wd.findElement(By.name("selected[]")).click();
+    wd.findElement(By.name("delete")).click();
+    wd.findElement(By.linkText("group page")).click();
+    }
+
+
+
+  }
+
+
+
