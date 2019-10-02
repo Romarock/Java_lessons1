c470fec08c9d3fd2165a3f9dcb3278c72ff41844
Romarock <55085243+Romarock@users.noreply.github.com>
2019-10-02 13:57:05 +0300

    добавлен выбор группы по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -16,7 +16,7 @@ public class GroupModificationTests extends TestBase {
 
         }
         int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
         app.getGroupHelper().submitGroupModification();
