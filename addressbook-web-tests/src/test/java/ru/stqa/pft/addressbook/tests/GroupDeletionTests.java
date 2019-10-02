ba7c2b89a1ebdd577d1b48da6d697489f02cd5fe
Romarock <55085243+Romarock@users.noreply.github.com>
2019-10-02 17:25:35 +0300

    Добавлена проверка списков групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -29,7 +29,15 @@ public class GroupDeletionTests extends TestBase {
     List<GroupData> after= app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
 
+    before.remove(before.size() -1);
+
+
+      Assert.assertEquals(before, after);
+
+
     }
+
+
 
 
 }
