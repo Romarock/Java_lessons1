27471db43c9ecfa8c364c95de09a5c424dc6b821
Romarock <55085243+Romarock@users.noreply.github.com>
2019-10-03 16:55:19 +0300

    Добавлен поиск максимального элемента

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -75,7 +75,8 @@ public class GroupHelper extends HelperBase {
         List<GroupData> groups = new ArrayList<GroupData>();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {String name = element.getText();
-            GroupData group = new GroupData(name, null, null);
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            GroupData group = new GroupData(name, null, null, id);
             groups.add(group);
         }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+
+
+
+    private  int id;
     private final String name;
     private final String header;
     private final String footer;
@@ -11,6 +15,14 @@ public class GroupData {
         this.name = name;
         this.header = header;
         this.footer = footer;
+        this.id = 0;
+    }
+
+    public GroupData(String name, String header, String footer, int id) {
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+        this.id = id;
     }
 
     public String getName() {
@@ -25,11 +37,12 @@ public class GroupData {
         return footer;
     }
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                '}';
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
@@ -37,11 +50,21 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return Objects.equals(id, groupData.id) &&
+                Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(id, name);
     }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
