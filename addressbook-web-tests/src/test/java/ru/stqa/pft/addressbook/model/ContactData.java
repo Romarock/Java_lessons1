0090541a415ca8ad808e6fc9a111b917b2495f7f
Romarock <55085243+Romarock@users.noreply.github.com>
2019-10-07 16:02:33 +0300

    на проверку

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,19 +3,33 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class ContactData {
+    private final String id;
     private final String name;
     private final String secondName;
     private final String phone;
     private final String email;
     private final String address;
 
-    public ContactData(String name, String secondName, String phone, String email, String address) {
+
+    public ContactData( String name, String secondName, String phone, String email, String address) {
+        this.id = null;
         this.name = name;
         this.secondName = secondName;
         this.phone = phone;
         this.email = email;
         this.address = address;
     }
+
+    public ContactData(String id, String name, String secondName, String phone, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.secondName = secondName;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getId() { return id;}
 
     public String getName() {
         return name;
@@ -38,24 +52,27 @@ public class ContactData {
     }
 
     @Override
-    public String toString() {
-        return "ContactData{" +
-                "name='" + name + '\'' +
-                ", secondName='" + secondName + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ContactData that = (ContactData) o;
-        return Objects.equals(name, that.name) &&
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
                 Objects.equals(secondName, that.secondName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, secondName);
+        return Objects.hash(id, name, secondName);
     }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", secondName='" + secondName + '\'' +
+                '}';
+    }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -20,13 +22,21 @@ public class ContactModificationTests extends TestBase {
 
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().contactSelect(before.size() - 1);
+        app.getContactHelper().contactSelect(before.size() -1);
         app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactsFields(new ContactData("Wader", "Darth", "66666", "papavaider@sith.com", "Hot"));
+        ContactData contact = new ContactData((before.get(before.size()-1).getId()),"Wader", "Darth", "66666", "papavaider@sith.com", "Hot");
+        app.getContactHelper().fillContactsFields(contact);
         app.getContactHelper().SubmitContactModification();
         app.getNavigationHelper().goToHomePage();
 
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(contact);
+        System.out.println(new HashSet<Object>(before));
+        System.out.println(new HashSet<Object>(after));
+
+      //  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
