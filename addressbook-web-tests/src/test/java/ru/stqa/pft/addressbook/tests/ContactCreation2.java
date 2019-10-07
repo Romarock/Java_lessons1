95528a3e7cb4a0c78d03b7cf12a59109a990f4cd
Romarock <55085243+Romarock@users.noreply.github.com>
2019-10-07 18:44:38 +0300

    Добавлено лямбда выражение для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation2.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -30,15 +29,8 @@ public class ContactCreation2 extends TestBase {
     Assert.assertEquals(after.size(), before.size() +1);
 
 
-  int max = 0;
-  for (ContactData g : after) {
 
-    if (g.getId() > max) {
-      max = g.getId();
-    }
-
-  }
-    contact.setId(max);
+   contact.setId( after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
   Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
